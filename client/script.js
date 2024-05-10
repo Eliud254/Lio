51a8f7d29208dd2c2aefa4f2db11f311ce88e729
eliud <eliudobure@gmail.com>
2024-05-10 20:15:14 +0300

    Another commit

--- client/script.js
+++ client/script.js
@@ -1,13 +1,15 @@
 document.getElementById('loginBtn').addEventListener('click', function() {
   var loginContainer = document.getElementById('loginContainer');
   var signupContainer = document.getElementById('signupContainer');
+  var pricingDashboard = document.getElementById('pricingDashboard');
   
   // Toggle visibility of login form
   if (loginContainer.classList.contains('show-login')) {
     loginContainer.classList.remove('show-login');
   } else {
     loginContainer.classList.add('show-login');
-    signupContainer.classList.remove('show-signup'); // Ensure signup form is hidden when showing login form
+    signupContainer.classList.remove('show-signup');
+    pricingDashboard.classList.remove('show-pricing');
   }
 });
 
@@ -15,12 +17,30 @@ document.getElementById('signupLink').addEventListener('click', function(event) 
   event.preventDefault(); // Prevent the default link behavior
   var signupContainer = document.getElementById('signupContainer');
   var loginContainer = document.getElementById('loginContainer');
+  var pricingDashboard = document.getElementById('pricingDashboard');
 
   // Toggle visibility of signup form
   if (signupContainer.classList.contains('show-signup')) {
-    signupContainer.classList.remove('show-signup');
+    signupContainer.classList.remove('show-signup');      
   } else {
     signupContainer.classList.add('show-signup');
-    loginContainer.classList.remove('show-login'); // Ensure login form is hidden when showing signup form
+    loginContainer.classList.remove('show-login');
+    pricingDashboard.classList.remove('show-pricing');
+  }
+});
+
+document.getElementById('pricingBtn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default link behavior
+  var pricingDashboard = document.getElementById('pricingDashboard');
+  var loginContainer = document.getElementById('loginContainer');
+  var signupContainer = document.getElementById('signupContainer');
+
+  // Toggle visibility of pricing dashboard
+  if (pricingDashboard.classList.contains('show-pricing')) {
+    pricingDashboard.classList.remove('show-pricing');      
+  } else {
+    pricingDashboard.classList.add('show-pricing');
+    loginContainer.classList.remove('show-login');
+    signupContainer.classList.remove('show-signup');
   }
 });
